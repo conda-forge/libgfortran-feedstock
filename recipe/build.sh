89f3e90d67cb5d905b1acdb24e0eda8598c7163f
lexual <lex.hider@bom.gov.au>
2017-03-08 12:56:44 +1100

    just package up system gfortran.
    Note, I believe the devtools package one is just referencing this one anyway.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-# from Continuum's build.sh
+# Originally from Continuum's build.sh
 
 LIB=$PREFIX/lib
 mkdir $LIB
 cd $LIB
 
-if [ $ARCH == '64' ]; then
-    cp /usr/local/lib64/libgfortran.so.3.0.0 .
-    ln -s libgfortran.so.3.0.0 libgfortran.so.3
-elif [ $ARCH == '32' ]; then
-    cp /usr/local/lib/libgfortran.so.3.0.0 .
-    ln -s libgfortran.so.3.0.0 libgfortran.so.3
-fi
-
-if [ `uname -m` == 'armv7l' ]; then
-    cp /usr/lib/arm-linux-gnueabihf/libgfortran.so.3.0.0 .
-    ln -s libgfortran.so.3.0.0 libgfortran.so.3
-fi
+cp /usr/lib64/libgfortran.so.3.0.0 .
+ln -s libgfortran.so.3.0.0 libgfortran.so.3

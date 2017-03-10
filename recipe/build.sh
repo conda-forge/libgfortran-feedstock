a0a8ca55e29d5d948c0990610db447862276a551
lexual <lex.hider@bom.gov.au>
2017-03-11 00:08:33 +1100

    skip build.sh on non-Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Originally from Continuum's build.sh
 
-LIB=$PREFIX/lib
-mkdir $LIB
-cd $LIB
-
-cp /usr/lib64/libgfortran.so.3.0.0 .
-ln -s libgfortran.so.3.0.0 libgfortran.so.3
+if [ $(uname) == Linux ]; then
+    LIB=$PREFIX/lib
+    mkdir $LIB
+    cd $LIB
+    
+    cp /usr/lib64/libgfortran.so.3.0.0 .
+    ln -s libgfortran.so.3.0.0 libgfortran.so.3
+fi
